2c62222aae0808279fe415e6b7cc5fbb43b69419
Gustavo Muro <gustmuro@gmail.com>
2021-09-16 11:49:02 -0300

    work in task example

--- source/freertos_hello.c
+++ source/freertos_hello.c
@@ -1,36 +1,38 @@
-/*
- * The Clear BSD License
- * Copyright (c) 2015, Freescale Semiconductor, Inc.
- * Copyright 2016-2017 NXP
+/* Copyright 2017, DSI FCEIA UNR - Sistemas Digitales 2
+ *    DSI: http://www.dsi.fceia.unr.edu.ar/
+ * Copyright 2017, Diego Alegrechi
+ * Copyright 2017, Gustavo Muro
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided
- *  that the following conditions are met:
  *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
- * o Neither the name of the copyright holder nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
  */
+
+/*==================[inclusions]=============================================*/
 
 /* FreeRTOS kernel includes. */
 #include "key.h"
@@ -50,21 +52,49 @@
 #include "adc.h"
 #include "mma8451.h"
 
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
+/*==================[macros and definitions]=================================*/
 
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
-static void blinky_task(void *pvParameters);
+/*==================[internal functions declaration]=========================*/
 
-/*******************************************************************************
- * Code
- ******************************************************************************/
-/*!
- * @brief Application entry point.
- */
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+static void task1(void *pvParameters)
+{
+    for (;;)
+    {
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+        board_setLed(BOARD_LED_ID_ROJO, BOARD_LED_MSG_TOGGLE);
+    }
+}
+
+static void task2(void *pvParameters)
+{
+    for (;;)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        printf("Task 2\n");
+    }
+}
+
+static void task3(void *pvParameters)
+{
+    int i;
+
+    for (;;)
+    {
+    	i = 100000;
+    	while (i--);
+    	board_setLed(BOARD_LED_ID_VERDE, BOARD_LED_MSG_TOGGLE);
+    }
+}
+
+/*==================[external functions definition]==========================*/
 
 int main(void)
 {
@@ -77,24 +107,13 @@ int main(void)
 
     key_init();
 
-    xTaskCreate(blinky_task, "blinky_task", 300, NULL, 1, NULL);
+    xTaskCreate(task1, "task1", 300, NULL, 2, NULL);
+    xTaskCreate(task2, "task2", 300, NULL, 1, NULL);
+    xTaskCreate(task3, "task3", 300, NULL, 0, NULL);
 
     vTaskStartScheduler();
 
     for (;;);
-}
-
-/*!
- * @brief Task responsible for printing of "Hello world." message.
- */
-static void blinky_task(void *pvParameters)
-{
-    for (;;)
-    {
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-
-        board_setLed(BOARD_LED_ID_ROJO, BOARD_LED_MSG_TOGGLE);
-    }
 }
 
 void vApplicationTickHook(void)
@@ -108,10 +127,4 @@ extern void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName 
 }
 
 
-
-
-
-
-
-
-
+/*==================[end of file]============================================*/
